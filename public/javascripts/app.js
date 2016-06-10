0b39a6c81faa834b10d66bf0784b6033de284133
mohamedelkadi <molhm66@gmail.com>
2016-06-11 01:09:07 +0200

    make simple routing at the front end

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,17 +1,47 @@
 /**
  * Created by molhm on 6/10/16.
  */
-var source = $("#list-posts-template").html();
-var template = Handlebars.compile(source);
+var templates = [];
+templates['posts'] = Handlebars.compile($("#list-posts-template").html());
+templates['post'] = Handlebars.compile($("#show-post-template").html());
 
-$.ajax({
-    'url': '/posts',
-    'method': 'GET',
-    'accepts': 'application/json'
-}).then(function (data) {
-    var context = {
-        "posts": data
-    }
-    var html = template(context);
-    $('#posts').html(html);
-})
+function fetchAndShow(url, selector, template) {
+    console.log(url, selector, template);
+    $.ajax({
+        'url': url,
+        'method': 'GET',
+        'accepts': 'application/json'
+    }).then(function (data) {
+        var context = {}
+        if (Array.isArray(data)) {
+            context[template] = data;
+
+        } else {
+            context = data;
+        }
+        var html = templates[template](context);
+        $('#posts').html("");
+        $(selector).html(html);
+    })
+
+}
+function hashRouter(event) {
+
+    var url = (window.location.hash || '').replace(/^#/, '');
+    if (url == '')
+        return;
+    var parts = url.split('/');
+    var selector = '#' + parts[0];
+    if (parts[1])
+        template = 'post';
+    else
+        template = 'posts';
+    fetchAndShow(url, selector, template);
+}
+$(window).on('load', hashRouter);
+
+$(window).on('hashchange',
+    hashRouter
+);
+
+fetchAndShow('/posts', '#posts', 'posts');
